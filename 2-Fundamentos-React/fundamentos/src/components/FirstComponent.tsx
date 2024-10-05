60d20eb73b7cf469a7ac63035da9ac4dd26b13b2
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-05 13:04:38 -0300

    feat: first component

---
+++ 2-Fundamentos-React/fundamentos/src/components/FirstComponent.tsx
@@ -0,0 +1,14 @@
+function FirstComponent(){
+
+
+    return(
+        <div>
+            <h1>Meu primeiro componente</h1>
+            <button onClick={() => window.alert("Que bacanaa")}>CLique aqui</button>
+        </div>
+    )
+
+    
+}
+
+export default FirstComponent;
