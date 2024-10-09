94e1910c04ed592b7bb2d50ea2bc09ea4e955b16
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-09 20:07:16 -0300

    feat/imageExamples

--- 3-Avancado-Em-React/ImageProject/src/App.tsx
+++ 3-Avancado-Em-React/ImageProject/src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
+import vovo from "./assets/vovoMax.jfif" 
 
 function App() {
 
   return (
     <>
      <h1>Avançando em React</h1>
+     {/* imagem em public */}
+     <div>
+        <img src='/ben10.jfif' alt='ben 10'/>
+     </div>
+
+     {/* imagem em assets */}
+    <div>
+      <img src={vovo} alt="" />
+    </div>
     </>
   )
 }
