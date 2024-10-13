ad47a7d5ccbb14ed70022a796e85f352175e1f1d
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-13 17:52:15 -0300

    feat: prevState

--- 3-Avancado-Em-React/ImageProject/src/components/ListRender.tsx
+++ 3-Avancado-Em-React/ImageProject/src/components/ListRender.tsx
@@ -4,12 +4,23 @@ const ListRender = () => {
 
     const [list] = useState(["Matheus", "Jojo", "Lucas", "Jõao" ]);
 
-    const [users] = useState([
+    const [users, setUsers] = useState([
         {id: 1, Name:"Lapa", age:19},
         {id: 2, Name:"Jõao", age:17},
         {id: 3, Name:"Pablo", age:20},
         {id: 4, Name:"Pedro", age:21},
     ])
+
+
+    function deleteRandom(){
+        const randomNumber = Math.floor(Math.random() * 5);
+
+        setUsers((prevUsers) => { /*prevUsers é a lista antes de ser modificada, dessa forma ele retorna no final do código a lista com um filter para só tirar um id aleatorio */
+            console.log(prevUsers)
+            return prevUsers.filter((user) => randomNumber !== user.id)
+        })
+
+    };
 
   return (
     <div>
@@ -31,6 +42,7 @@ const ListRender = () => {
             })}
 
         </ul>
+        <button onClick={deleteRandom}>deletar</button>
     </div>
   )
 }
