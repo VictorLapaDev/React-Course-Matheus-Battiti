a1cdcce5705036c7eef7b326f798abc181403fb5
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-13 17:39:02 -0300

    feat: key no metodo map

--- 3-Avancado-Em-React/ImageProject/src/components/ListRender.tsx
+++ 3-Avancado-Em-React/ImageProject/src/components/ListRender.tsx
@@ -4,15 +4,32 @@ const ListRender = () => {
 
     const [list] = useState(["Matheus", "Jojo", "Lucas", "Jõao" ]);
 
+    const [users] = useState([
+        {id: 1, Name:"Lapa", age:19},
+        {id: 2, Name:"Jõao", age:17},
+        {id: 3, Name:"Pablo", age:20},
+        {id: 4, Name:"Pedro", age:21},
+    ])
 
   return (
     <div>
         <ul>
-            {list.map((item) => {
+            {list.map((item, i) => {
                 return(
-                    <li>{item}</li>
+                    <li key={i}>{item}</li>
                 )
             })}
+        </ul>
+
+        <ul>
+            {users.map((user) => {
+                return(
+                    <li key={user.id}>
+                        Nome: {user.Name}, idade: {user.age}
+                    </li>
+                )
+            })}
+
         </ul>
     </div>
   )
