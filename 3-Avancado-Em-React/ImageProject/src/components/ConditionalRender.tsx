c144abe7c4596502e28eb11963af401c209b22c3
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-13 18:14:50 -0300

    feat: operador ternario

--- 3-Avancado-Em-React/ImageProject/src/components/ConditionalRender.tsx
+++ 3-Avancado-Em-React/ImageProject/src/components/ConditionalRender.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 const ConditionalRender = () => {
 
-    const [x] = useState(true)
+    const [x, setX] = useState(true)
 
   return (
     <div>
@@ -10,6 +10,21 @@ const ConditionalRender = () => {
         {x &&  <p>Sim, isso irá aparecer pois x é true</p>} {/*condicional simples*/}
         {!x && <p>Isso irá aparecer pois x é falso</p>}
 
+        
+        {x == true ? (
+            <div>
+                <p>Sim, isso irá aparecer pois x é true</p>
+            </div>
+            ) : (
+            <div>
+                <p>Isso irá aparecer pois x é falso</p>
+            </div>
+            )
+        }
+        
+        <button onClick={() => setX(prevX => !prevX)}>
+            Mudar o X = {x.toString()} {/* Converte x para string para exibição */}
+        </button>
     </div>
   )
 }
