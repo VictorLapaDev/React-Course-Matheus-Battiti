cb5812af133fb688a83e1b8d4f4c33bba091ec85
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-15 19:58:42 -0300

    feat: css em componentes

--- 4-CSS/react-css/src/App.tsx
+++ 4-CSS/react-css/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
 
   return (
     <>
+    {/* CSS global */}
       <h1>React com css</h1>
+      <p>Css global</p>
+    {/* CSS em componente */}
+    <MyComponent/>
     </>
   )
 }
---
+++ 4-CSS/react-css/src/components/MyComponent.tsx
@@ -0,0 +1,13 @@
+import "./MyComponent.css"
+
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1 className="my-comp-h1">CSS de componente</h1>
+        <p className="my-comp-para">Este é um estilo feito para dentro do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent
