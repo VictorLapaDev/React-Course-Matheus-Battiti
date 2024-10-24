b33301e102f4c950036e326ee9b3922e475b3705
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-24 19:21:26 -0300

    feat: controlled inputs

--- 5-Formularios/Forms/src/components/forms/Forms.tsx
+++ 5-Formularios/Forms/src/components/forms/Forms.tsx
@@ -2,13 +2,17 @@
 import { useState } from "react"
 import "./Forms.css"
 
+interface props{
+  user: object
+}
 
-const Forms = () => {
+const Forms = ({user}: props) => {
+
+  // 6- controlled inputs
 
   // 3- Gerenciamento de dados
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
+  const [name, setName] = useState(user ? user.name : '');
+  const [email, setEmail] = useState(user ? user.email : ''); //ve se usuario ja existe ou se ele ja tem o valor armazenado, preenchendo diretamente o input
   const handleName = (e) => {
     setName(e.target.value)
   }
@@ -26,13 +30,13 @@ const Forms = () => {
         <form onSubmit={handleSubmit}>
             <div className="container">
                 <label htmlFor="name">Nome: </label>
-                <input type="text" name="name" placeholder="Digite seu nome" onChange={handleName}/>
+                <input type="text" name="name" placeholder="Digite seu nome" onChange={handleName} value={name}/>
             </div>
 
             {/* 2-label envolvendo input */}
             <label className="container">
               <span>Emai:</span>
-              <input type="text" name='email' placeholder="Digite seu email" onChange={(e) => setEmail(e.target.value)}/>
+              <input type="text" name='email' placeholder="Digite seu email" onChange={(e) => setEmail(e.target.value)} value={email}/>
             </label>
 
 
