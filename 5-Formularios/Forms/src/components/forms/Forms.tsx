7557ae3527b42bcd8095ec3db4d420114bfffc65
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-24 11:03:30 -0300

    feat:gerenciamento de dados de input

--- 5-Formularios/Forms/src/components/forms/Forms.tsx
+++ 5-Formularios/Forms/src/components/forms/Forms.tsx
@@ -1,13 +1,26 @@
+
+import { useState } from "react"
 import "./Forms.css"
 
+
 const Forms = () => {
+
+  // 3- Gerenciamento de dados
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+  
+  
   return (
     <div>
         {/*1-criação de forms*/}
         <form>
             <div className="container">
                 <label htmlFor="name">Nome: </label>
-                <input type="text" name="name" placeholder="Digite seu nome"/>
+                <input type="text" name="name" placeholder="Digite seu nome" onChange={handleName}/>
             </div>
 
             {/* 2-label envolvendo input */}
