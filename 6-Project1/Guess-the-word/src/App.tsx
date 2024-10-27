49dc02d6e9b8ed4f448b4de50851282e1615089c
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-26 22:04:08 -0300

    feat: creating stages of game

--- 6-Project1/Guess-the-word/src/App.tsx
+++ 6-Project1/Guess-the-word/src/App.tsx
@@ -1,12 +1,36 @@
 
 import './App.css'
+
 import StartScreen from './components/StartScreen/StartScreen'
+import Game from './components/Game/Game'
+import End from './components/End/End'
+
+import { useCallback, useState, useEffect } from 'react'
+
+import {word_list} from './data/words'
+
+
+const stages = [
+  {id: 1, name: 'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'}
+]
+
 
 function App() {
 
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(word_list);
+
+  console.log(words);
+
   return (
     <div className='app'>
-      <StartScreen/>
+
+      {gameStage === 'start' && <StartScreen/>}
+      {gameStage === 'game' && <Game/>}
+      {gameStage === 'end' && <End/>}
+
     </div>
   )
 }
---
+++ 6-Project1/Guess-the-word/src/components/End/End.tsx
@@ -0,0 +1,11 @@
+import './End.css'
+
+const End = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default End
