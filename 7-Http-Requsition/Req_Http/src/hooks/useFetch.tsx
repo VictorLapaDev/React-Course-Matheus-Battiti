56c080b1ba469fbfe13721a09dac3f34a20daa9b
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-29 19:05:12 -0300

    feat: post no custom hook(nao funciona em tsx)

--- 7-Http-Requsition/Req_Http/src/hooks/useFetch.tsx
+++ 7-Http-Requsition/Req_Http/src/hooks/useFetch.tsx
@@ -1,10 +1,31 @@
 import { useEffect, useState } from "react"
 
 
+
+
 //4- custom hook
 export const useFetch = (url: string) => {
-    
+
     const [data, setData] = useState('');
+
+    //5- refatorando post no custom hook
+    // const [config, setConfig] = useState();
+    // const [method, setMethod] = useState();
+    // const [callFetch, setCallFetch] = useState(false);
+
+
+    // const httpConfig = (data: any, method: string) => {
+    //     if (method === "POST") {
+    //       setConfig({
+    //         method: "POST",
+    //         headers: {
+    //           "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify(data),
+    //       });
+    
+    //       setMethod("POST")
+    //     }
 
     useEffect(() => {
 
@@ -16,8 +37,27 @@ export const useFetch = (url: string) => {
         }
 
         fetchData();
-    }, [url])
+    }, [url, {/*callFetch*/}])
 
+
+        //5- refatorando post no custom hook
+        // useEffect(() => {
+
+        //     const httpRequest = async () => {
+        //         if (method === "POST") {
+                   
+        //           // 5 - refatorando post
+        //           let fetchOptions = [url, config];
+          
+        //           const res = await fetch(...fetchOptions);
+          
+        //           const json = await res.json();
+          
+        //           setCallFetch(json)
+        //         }
+
+        //     httpRequest(); 
+        // }, [config])
 
     return { data };
 }
