fe6fd7d588ce08616e798e460d798f661a32278d
VictorLapaDev <victorlucaslapa@gmail.com>
2024-10-29 11:37:32 -0300

    feat: consuming api teste

--- 7-Http-Requsition/Req_Http/src/App.tsx
+++ 7-Http-Requsition/Req_Http/src/App.tsx
@@ -1,7 +1,28 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 
+const url = 'http://localhost:3000/products'
+
 function App() {
+
+
+  const[products, setProducts] = useState([]);
+
+    //1- Resgatando dados(fetch)
+    useEffect(async() => {
+
+        const response = await fetch(url) //pega os dados de dentro da url
+
+        const data = await response.json() //pega os dados json e transforma em objeto
+
+        setProducts(data) //seta os dados
+      
+        //é feito dentro do useEffect para chamar a requisição só uma vez
+    }, [])
+
+    console.log(products)
+
 
   return (
     <>
