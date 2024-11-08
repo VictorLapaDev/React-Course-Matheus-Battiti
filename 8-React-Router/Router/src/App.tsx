c319ccd95ba4681ad97a35cf6f607a41d941b21c
VictorLapaDev <victorlucaslapa@gmail.com>
2024-11-08 20:22:03 -0300

    feat: configuring react router and making the navigation between the pages

--- 8-React-Router/Router/src/App.tsx
+++ 8-React-Router/Router/src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
 
+
+//1- config react-router
+import { Routes, BrowserRouter, Route } from 'react-router-dom'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import NavBar from './assets/components/NavBar/NavBar'
+
 function App() {
 
   return (
     <>
      <h1>React Router</h1>
+
+     <BrowserRouter>
+       <NavBar/>{/* Como tem componentes do react router ele tem que ser renderizado aqui  */}
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+/     </Routes>
+     </BrowserRouter>
     </>
   )
 }
