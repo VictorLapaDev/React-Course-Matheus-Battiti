dcee5e49c39cf023dff742b8a64b87e0ea7d4b1f
VictorLapaDev <victorlucaslapa@gmail.com>
2024-11-12 18:01:15 -0300

    feat: NavLink ou link ativo, navbar

--- 8-React-Router/Router/src/components/NavBar/NavBar.tsx
+++ 8-React-Router/Router/src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './NavBar.css'
 
 
@@ -6,8 +6,17 @@ import './NavBar.css'
 const NavBar = () => {
   return (
     <nav>
-      <Link to={'/'}>Home</Link>
-      <Link to={'/about'}>About</Link>
+      {/* <Link to={'/'}>Home</Link>
+      <Link to={'/about'}>About</Link> */}
+      
+      {/* 8- nav link  */}
+      <NavLink to={'/'} 
+      // className={( { isActive }) => (isActive ? 'ativo' : 'naoAtivo')} {/* Assim consigo fazer um ternario para ele chamar uma classe diferente se esta ativo ou não*/}
+      >Home</NavLink> 
+
+      <NavLink to={'/about'}>About</NavLink>
+
+      {/* o active no css muda como o componente fica ao ativado */}
     </nav>
   )
 }
