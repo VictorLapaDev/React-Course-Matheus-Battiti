1befcf1717e0f4901918a73c189efbd9a4e5ae5b
unknown <victorlucaslapa@gmail.com>
2025-01-24 16:40:03 -0300

    feat: showing results from search

--- 10-Mini-Blog/MiniBlog/src/pages/Search/Search.tsx
+++ 10-Mini-Blog/MiniBlog/src/pages/Search/Search.tsx
@@ -1,20 +1,36 @@
-import styles from './Search.module.css'
+import styles from "./Search.module.css";
 
 //hooks
-import { useFetchDocuments } from '../../hooks/useFetchDocuments'
-import { useQuery } from '../../hooks/useQuery'
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useQuery } from "../../hooks/useQuery";
+import PostDetails from "../../components/PostDetails/PostDetails";
+import { Link } from "react-router-dom";
 
 const Search = () => {
+  const query = useQuery();
+  const search = query.get("q");
 
-    const query = useQuery()
-    const search = query.get('q')
+  const { documents: posts } = useFetchDocuments("posts", search);
 
   return (
-    <div>
-        <h2>Search</h2>
-        <p>{search}</p>
-    </div>
-  )
-}
+    <div className={styles.search_container}>
+      <h2>Search</h2>
+      <div>
 
-export default Search
+        {!posts && (
+            <div className={styles.noposts}>
+                <p>Não foram encontrados posts a partir de sua busca...</p>
+                <Link to="/" className="btn btn-dark">Voltar</Link>
+            </div>
+        )}
+
+        {posts && posts.map((post) => (
+          <PostDetails key={post.id} post={post} />
+        ))}
+        
+      </div>
+    </div>
+  );
+};
+
+export default Search;
