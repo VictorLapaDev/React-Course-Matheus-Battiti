c8301669f5ff3c130228bf079483cca9ed98b39b
unknown <victorlucaslapa@gmail.com>
2025-01-26 13:19:11 -0300

    feat: finish structuring dashboard page

--- 10-Mini-Blog/MiniBlog/src/pages/Dashboard/Dashboard.tsx
+++ 10-Mini-Blog/MiniBlog/src/pages/Dashboard/Dashboard.tsx
@@ -1,46 +1,64 @@
-import style from './Dashboard.module.css'
+import style from "./Dashboard.module.css";
 
 //hooks
-import { useAuthValue } from "../../context/AuthContext"
-import { useFetchDocuments } from '../../hooks/useFetchDocuments';
-import { Link } from 'react-router-dom';
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
-
   const { user } = useAuthValue();
   const uid = user.uid;
 
   //posts do usuario
-  const {documents: posts, loading} = useFetchDocuments("posts", null, uid);
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
+  const deleteDocument = (id) => {
 
+  }
+
+  if(loading){
+    return <p>Carregando...</p>
+  }
   return (
     <div>
-        <h2>Dashboard</h2>
-        <p>Gerencie seus posts</p>
+      <h2>Dashboard</h2>
+      <p>Gerencie seus posts</p>
 
-
-
-        {posts && posts.length === 0 ? (
-            <div className={style.nopost}>
-              <p>Não foram encontrados posts</p>
-              <Link to="/posts/create" className='btn'>
-                  Crie seu primeio post
-              </Link>
-            </div>
-        ) : (
+      {posts && posts.length === 0 ? (
+        <div className={style.nopost}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Crie seu primeio post
+          </Link>
+        </div>
+      ) : (
+        <div>
           <div>
-            <p>Tem posts!!!</p>
+            <span>Titulo</span>
+            <span>Ações</span>
           </div>
-        )}
-
-        {loading && <p>Carregando posts...</p>}
-
-        {posts && posts.map((post) => (
-              <p key={post}>{post.title} </p>
+          {posts &&
+            posts.map((post) => (
+              <div key={post.id}>
+                <p>{post.title}</p>
+                <div>
+                  <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                    Ver post
+                  </Link>
+                  <Link to={`posts/edit/${post.id}`} className="btn btn-outline">
+                    Editar
+                  </Link>
+                  <button onClick={() => deleteDocument(post.id)} className="btn btn-outline btn-danger">
+                    Excluir
+                  </button>
+                </div>
+              </div>
             ))}
-    </div>
-  )
-}
+        </div>
+      )}
 
-export default Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
