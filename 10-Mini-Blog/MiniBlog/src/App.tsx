14d182a90bf07c02dbd3fed6b19e75fc3ca179d9
unknown <victorlucaslapa@gmail.com>
2025-01-26 11:35:38 -0300

    feat: create individual post page

--- 10-Mini-Blog/MiniBlog/src/App.tsx
+++ 10-Mini-Blog/MiniBlog/src/App.tsx
@@ -21,6 +21,7 @@ import Login from "./pages/Login/Login";
 import CreatePost from "./pages/CreatePost/CreatePost.js";
 import Dashboard from "./pages/Dashboard/Dashboard.js";
 import Search from "./pages/Search/Search.js";
+import Post from "./pages/Post/Post.js";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -46,6 +47,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/search" element={<Search />} />
+            <Route path="/posts/:id" element={<Post/>} />
             <Route
               path="/login"
               element={!user ? <Login /> : <Navigate to={"/"} />}
