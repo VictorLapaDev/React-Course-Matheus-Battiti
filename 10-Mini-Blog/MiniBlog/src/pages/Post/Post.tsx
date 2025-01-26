d2666d61c5c675efc0041b50315e4b715ee58944
unknown <victorlucaslapa@gmail.com>
2025-01-26 12:23:49 -0300

    feat: create css from unique post page

--- 10-Mini-Blog/MiniBlog/src/pages/Post/Post.tsx
+++ 10-Mini-Blog/MiniBlog/src/pages/Post/Post.tsx
@@ -16,9 +16,22 @@ const Post = () => {
         )}
 
         {post && (
-            <>
+            <div className={styles.post_container}>
                 <h1>{post.title}</h1>
-            </>
+                <img src={post.image} alt={post.title}/>
+                <p>{post.body}</p>
+                <h3>Esse post trata sobre:</h3>
+
+                <div className={styles.tags}>
+                    {post.tags.map((tag) => (
+                            <p key={tag}>
+                                <span>#</span>
+                                {tag}
+                            </p>
+                    ))}
+                </div>
+                
+            </div>
         )}
 
     </div>
