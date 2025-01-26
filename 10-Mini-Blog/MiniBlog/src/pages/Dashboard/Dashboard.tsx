6b5dee17f88aa54f772778abd57ad31b2f5e05ae
unknown <victorlucaslapa@gmail.com>
2025-01-26 12:45:20 -0300

    feat: structuring dashboard

--- 10-Mini-Blog/MiniBlog/src/pages/Dashboard/Dashboard.tsx
+++ 10-Mini-Blog/MiniBlog/src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,38 @@
 import style from './Dashboard.module.css'
 
+//hooks
+import { useAuthValue } from "../../context/AuthContext"
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { Link } from 'react-router-dom';
+
 const Dashboard = () => {
+
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  //posts do usuario
+  const posts = []
+
+
+
   return (
     <div>
-        <h1>Dashboard</h1>
+        <h2>Dashboard</h2>
+        <p>Gerencie seus posts</p>
+
+        {posts && posts.length === 0 ? (
+            <div className={style.nopost}>
+              <p>Não foram encontrados posts</p>
+              <Link to="/posts/create" className='btn'>
+                  Crie seu primeio post
+              </Link>
+            </div>
+        ) : (
+          <div>
+            
+          </div>
+        )}
+
     </div>
   )
 }
