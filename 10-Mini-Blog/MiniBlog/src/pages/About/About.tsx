b211672080090d36a31bd12fe34c5eec31e61dd3
VictorLapa <141652519+VictorLapaDev@users.noreply.github.com>
2025-01-29 06:09:04 -0300

    fix: fixing a link

--- 10-Mini-Blog/MiniBlog/src/pages/About/About.tsx
+++ 10-Mini-Blog/MiniBlog/src/pages/About/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
       <p>Este projeto consiste em um blog feito com react no front end e firebase no back end, com base nas aulas do curso "React do Zero a Maestria (c/ hooks, router, API, Projetos)" do professor Matheus Battisti na udemy </p>
 
       <p className={styles.acesse}> Acesse :
-         <a className={styles.link}>https://www.udemy.com/course/react-do-zero-a-maestria-c-hooks-router-api-projetos/?couponCode=ST16MT28125</a>
+         <a className={styles.link} href="https://www.udemy.com/course/react-do-zero-a-maestria-c-hooks-router-api-projetos/?couponCode=ST16MT28125" target="_blank">https://www.udemy.com/course/react-do-zero-a-maestria-c-hooks-router-api-projetos/?couponCode=ST16MT28125</a>
       </p>
       
       <Link to="/posts/create" className="btn"> Criar Post</Link>
