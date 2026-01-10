89bf45b3808069daeb92a33c7c2529afc05fcc1e
unknown <victorlucaslapa@gmail.com>
2026-01-10 10:15:26 -0300

    feat: useEffect com array de dependencias vazio

--- 11-Hooks/HooksReact/src/components/HookUseEffect.jsx
+++ 11-Hooks/HooksReact/src/components/HookUseEffect.jsx
@@ -9,6 +9,13 @@ const HookUseEffect = () => {
         console.log("Estou sendo executado")
     })
 
+    // 2 - useEffect com array de dependencia vazio
+
+    //só é executado uma vez ao renderizar a página, independente do que mude o valor agora, ele não é exexecutado
+    useEffect(() => {
+        console.log("Serei executado apenas uma vez!")
+    }, [])
+
     const [num, setNum] = useState(0);
 
     const changeSomething = () => {
