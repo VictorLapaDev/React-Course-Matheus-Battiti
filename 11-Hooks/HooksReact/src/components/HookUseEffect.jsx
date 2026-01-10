c64e2d025a5b371b593e16ccb944760360a24a76
unknown <victorlucaslapa@gmail.com>
2026-01-10 10:34:04 -0300

    feat: useEffect com array de dependencia

--- 11-Hooks/HooksReact/src/components/HookUseEffect.jsx
+++ 11-Hooks/HooksReact/src/components/HookUseEffect.jsx
@@ -3,31 +3,42 @@ import {useEffect, useState} from 'react'
 const HookUseEffect = () => {
 
     // 1 - useEffect sem dependencias
-
     //toda vez que alguma variavel muda dentro do nosso escopo, ele executa o useEffect
+    const [num, setNum] = useState(0);
+
     useEffect(() => {
         console.log("Estou sendo executado")
     })
+    
 
-    // 2 - useEffect com array de dependencia vazio
-
-    //só é executado uma vez ao renderizar a página, independente do que mude o valor agora, ele não é exexecutado
+    // 2 - useEffect com array de dependencia vazio 
+    //só é executado uma vez ao renderizar a página, independente do que mude o valor agora, ele não é executado
     useEffect(() => {
         console.log("Serei executado apenas uma vez!")
     }, [])
 
-    const [num, setNum] = useState(0);
+    // 3 - useEffect com array de dependencias 
+    // ele executa ao carregar a pagina e apenas se o valor dentro do array de dependencia dele for alterado    
+    
+    const [otherNum, setOtherNum] = useState(0);
 
-    const changeSomething = () => {
-        setNum(num + 1)
-    }
+    useEffect(() => {
+        console.log("Sou executado apenas quando otherNum muda!");
+    }, [otherNum]);
+
+    
+
+    //Detalhe, o useEffect é sempre executado ao carregar a página, independente de qual tipo você utilizar.
 
   return (
     <div>
         <h2>useEffect</h2>
 
         <p>{num}</p>
-        <button onClick={changeSomething}>+</button>
+        <button onClick={() => setNum(num + 1)}>+ 1</button>
+
+        <p>{otherNum}</p>
+        <button onClick={() => setOtherNum(otherNum + 2)}>+ 2</button>
         <hr />  
     </div>
   )
