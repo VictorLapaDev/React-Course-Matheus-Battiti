c2d82d7bee61ae8b82a10606637e30b707c31435
unknown <victorlucaslapa@gmail.com>
2026-01-17 20:29:14 -0300

    feat: formatando código e adicionando user model

--- 13-ReactGram/backend/app.js
+++ 13-ReactGram/backend/app.js
@@ -7,32 +7,27 @@ const cors = require("cors");
 const port = process.env.PORT;
 
 const app = express();
- 
+
 //config JSON and form data response
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // Solve cors
-app.use(cors({credentials: true, origin: "http://localhost:5173"})) //onde o front end fica, porta do react vite 5173 //Permite que o frontend acesse o backend mesmo estando em outro domínio/porta
+app.use(cors({ credentials: true, origin: "http://localhost:5173" })); //onde o front end fica, porta do react vite 5173 //Permite que o frontend acesse o backend mesmo estando em outro domínio/porta
 
 // Upload directory
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // DB connection
-require("./config/db.js")
-
+require("./config/db.js");
 
 // routes
-const router = require("./routes/Router.js")
-app.use(router)
+const router = require("./routes/Router.js");
+app.use(router);
 
 app.listen(port, () => {
-    console.log(`App rodando na porta: ${port}`)
-})
-
-
-
-
+  console.log(`App rodando na porta: ${port}`);
+});
 
 //comandos utilizados para inicializar o projeto
 //npm init -y
--- 13-ReactGram/backend/config/db.js
+++ 13-ReactGram/backend/config/db.js
@@ -1,21 +1,22 @@
 const mongoose = require("mongoose");
 
 // Connection
-const  dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASS
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
 const conn = async () => {
-    try {
-        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@reactgram.i0rnwnl.mongodb.net/?appName=ReactGram`)
-        console.log("Conectou ao banco")
+  try {
+    const dbConn = await mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@reactgram.i0rnwnl.mongodb.net/?appName=ReactGram`,
+    );
+    console.log("Conectou ao banco");
 
-        return dbConn
-    } catch (error) {
-        console.log(error);
-    }
-}
+    return dbConn;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-conn()
+conn();
 
 module.exports = conn;
