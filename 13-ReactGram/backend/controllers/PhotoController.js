48d699bde386b05c6418b4121f75f4c36797ca16
unknown <victorlucaslapa@gmail.com>
2026-01-22 16:03:57 -0300

    feat: like em fotos

--- 13-ReactGram/backend/controllers/PhotoController.js
+++ 13-ReactGram/backend/controllers/PhotoController.js
@@ -131,11 +131,39 @@ const updatePhoto = async (req, res) => {
   res.status(200).json({photo, message: "Foto atualizada com sucesso"})
 };
 
+// Like function
+const likePhoto = async (req, res) => {
+  const {id} = req.params
+
+  const reqUser = req.user
+
+  const photo = await Photo.findById(id)
+
+   //check if photo exists
+  if (!photo) {
+    res.status(404).json({ errors: ["Foto não encontrada"] });
+    return;
+  }
+
+  // Check if user already liked
+  if(photo.likes.includes(reqUser._id)){
+    res.status(422).json({ errors: ["Você já curtiu a foto"] });
+    return
+  }
+
+  // Put user id in likes array
+  photo.likes.push(reqUser._id)
+  photo.save()
+  res.status(200).json({photoId: id, userId: reqUser._id, message: "A foto foi curtida"})
+
+}
+
 module.exports = {
   insertPhoto,
   deletePhoto,
   getAllPhotos,
   getUserPhotos,
   getPhotoById,
-  updatePhoto
+  updatePhoto,
+  likePhoto
 };
