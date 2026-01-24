a5980ed8b859bc62d78c2a270500780a8891b349
unknown <victorlucaslapa@gmail.com>
2026-01-24 10:44:15 -0300

    feat: criando navBar e footer

--- 13-ReactGram/frontend/src/App.jsx
+++ 13-ReactGram/frontend/src/App.jsx
@@ -3,6 +3,10 @@ import "./App.css";
 // Router
 import { Routes, BrowserRouter, Route, Navigate } from "react-router-dom";
 
+// Components
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
 // Pages
 import Home from "./pages/Home/Home";
 import Register from "./pages/Auth/Register";
@@ -12,11 +16,13 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
