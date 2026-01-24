0e9bcb21cf98b8993a7f3b69702673324b01305d
unknown <victorlucaslapa@gmail.com>
2026-01-24 11:05:19 -0300

    feat: css da navBar e footer

--- 13-ReactGram/frontend/src/components/NavBar.jsx
+++ 13-ReactGram/frontend/src/components/NavBar.jsx
@@ -18,15 +18,22 @@ const NavBar = () => {
 
       <form>
         <BsSearch />
-        <input type="text" />
+        <input type="text" placeholder="Pesquisar" />
       </form>
 
       <ul id="nav-links">
-        <NavLink to="/">
-          <BsHouseDoorFill />
-        </NavLink>
-        <NavLink to="/login">Entrar</NavLink>
-        <NavLink to="/register">Cadastrar</NavLink>
+        <li>
+          <NavLink to="/">
+            <BsHouseDoorFill />
+          </NavLink>
+        </li>
+        <li>
+          {" "}
+          <NavLink to="/login">Entrar</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Cadastrar</NavLink>
+        </li>
       </ul>
     </nav>
   );
