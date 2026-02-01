962a8b1b9d2e4aa9f2eac55ec9405b01d62fcea6
unknown <victorlucaslapa@gmail.com>
2026-02-01 10:26:37 -0300

    feat: exibindo validação do usuario

--- 13-ReactGram/frontend/src/pages/Auth/Register.jsx
+++ 13-ReactGram/frontend/src/pages/Auth/Register.jsx
@@ -2,6 +2,7 @@ import "./Auth.css";
 
 // Components
 import { Link } from "react-router-dom";
+import Message from "../../components/Message";
 
 // Hooks
 import { useState, useEffect } from "react";
@@ -69,7 +70,10 @@ const Register = () => {
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
         />
-        <input type="submit" value="Cadastrar" />
+
+        {!loading && <input type="submit" value="Cadastrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled/>}
+        {error && <Message msg={error} type="error"/>}
       </form>
       <p>Já tem conta? {<Link to={"/login"}>Clique aqui.</Link>}</p>
     </div>
--- 13-ReactGram/frontend/src/services/authServices.jsx
+++ 13-ReactGram/frontend/src/services/authServices.jsx
@@ -8,19 +8,19 @@ const register = async (data) => {
     const res = await fetch(api + "/users/register", config)
       .then((res) => res.json())
       .catch((err) => err);
-    
-      if(res){
-        localStorage.setItem("user", JSON.stringify(res))
-      }
 
+    if (res) {
+      localStorage.setItem("user", JSON.stringify(res));
+    }
 
+    return res;
   } catch (error) {
     console.log(error);
   }
 };
 
-const authService  = {
-    register
-}
+const authService = {
+  register,
+};
 
-export default authService
+export default authService;
